a52d94e35f0020a7e873a564c25456e37ed5acef
anup30 <anupbarua30@gmail.com>
2024-05-07 18:52:27 +0600

    commit 1.2

--- lib/screen_2.dart
+++ lib/screen_2.dart
@@ -1,6 +1,6 @@
 // https://www.youtube.com/watch?v=ZnZM8ot5lcc&list=PLJh8Hi_cW8DYP0LkOMzGA_lxRS2rYUi3k&index=5&ab_channel=AIwithFlutter // parts remaining
 // packages: flutter_map, latlong2
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
@@ -13,16 +13,52 @@ class Screen2 extends StatefulWidget {
 }
 
 class _Screen2State extends State<Screen2> {
+  final MapController _mapController = MapController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('flutter_map, latlong2'),backgroundColor: Colors.green[300],),
       body: content(),
+
+      floatingActionButton: buildFAB(),
+    );
+  }
+
+  Column buildFAB() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        const SizedBox(height: 10),
+        FloatingActionButton(
+          backgroundColor: Colors.green.withOpacity(0.4),
+          onPressed: () {
+            _mapController.move(_mapController.camera.center, _mapController.camera.zoom + 1);
+            setState(() {});
+          },
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 10),
+        FloatingActionButton(
+          backgroundColor: Colors.green.withOpacity(0.4),
+          onPressed: () {
+            _mapController.move(_mapController.camera.center, _mapController.camera.zoom - 1);
+            setState(() {});
+          },
+          child: const Icon(
+            Icons.remove,
+            color: Colors.white,
+          ),
+        ),
+      ],
     );
   }
 
   Widget content() {
     return FlutterMap(
+      mapController: _mapController,
       options: const MapOptions(
         initialCenter: LatLng(23.8041, 90.4152), //google, dhaka: 23.8041° N, 90.4152° E
         initialZoom: 11,
